48823eb5192e768a1342d7c77564fabc4adfe1b2
Valentin Dabovski <valentindabovski@val.fritz.box>
2023-01-13 17:46:12 +0100

    add db migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
