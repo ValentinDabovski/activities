07614d49492776890df25de819cd6d9b0eba4979
Valentin Dabovski <valentindabovski@val.fritz.box>
2023-01-13 17:11:36 +0100

    add sample project

---
+++ Domain/Address.cs
@@ -0,0 +1,32 @@
+namespace Domain
+{
+    public class Address : ValueObject
+    {
+        public Address(string street, string city, string state, string country, string zipcode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipcode;
+        }
+
+        public Address() { }
+
+        public String Street { get; private set; }
+        public String City { get; private set; }
+        public String State { get; private set; }
+        public String Country { get; private set; }
+        public String ZipCode { get; private set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            // Using a yield return statement to return each element one at a time
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+        }
+    }
+}
