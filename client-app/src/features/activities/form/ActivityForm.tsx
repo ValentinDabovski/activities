29a116f1eed262f34255329b7715891edad24d49
Valentin Dabovski <valentindabovski@val.fritz.box>
2023-02-03 16:05:50 +0100

    fix nested object input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -41,15 +41,27 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
         setActivity({ ...activity, [name]: value })
     }
 
+    function handAddressInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target
+        console.log(event.target)
+        setActivity({ ...activity, address: { ...activity.address, [name]: value } })
+    }
+
+    function handCategoryInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target
+        console.log(event.target)
+        setActivity({ ...activity, category: { ...activity.category, [name]: value } })
+    }
+
     return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
                 <Form.Input placeholder='Title' onChange={handleInputChange} value={activity.title} name='title' />
                 <Form.TextArea placeholder='Description' onChange={handleInputChange} value={activity.description} name='description' />
-                <Form.Input placeholder='Category' onChange={handleInputChange} value={activity.category} name='category' />
+                <Form.Input placeholder='Category' onChange={handCategoryInputChange} value={activity.category.name} name='name' />
                 <Form.Input type="date" placeholder='Date' onChange={handleInputChange} value={activity.date} name='date' />
-                <Form.Input placeholder='City' onChange={handleInputChange} value={activity.address.city} name='city' />
-                <Form.Input placeholder='Venue' onChange={handleInputChange} value={activity.address.venue} name='venue' />
+                <Form.Input placeholder='City' onChange={handAddressInputChange} value={activity.address.city} name='city' />
+                <Form.Input placeholder='Venue' onChange={handAddressInputChange} value={activity.address.venue} name='venue' />
                 <Button loading={submitting} basic floated="right" positive type="submit" content='Submit' />
                 <Button onClick={() => closeForm()} basic floated="right" type="button" content='Cancel ' />
             </Form>
