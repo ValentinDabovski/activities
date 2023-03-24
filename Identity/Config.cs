e2fa6314f1eacae33cdf573ccec956af775d1d23
Valentin Dabovski <valentindabovski@val.fritz.box>
2023-03-24 18:54:23 +0100

    add identity

---
+++ Identity/Config.cs
@@ -0,0 +1,35 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Identity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new()
+            {
+                Name = "activities-api",
+                DisplayName = "ActivitiesApi"
+            }
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new()
+            {
+                ClientId = "ActivitiesApi",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedScopes = { "activities-api" }
+            }
+        };
+}
